31149ddb2c5a8ca8bf08a3ea4f14bc43f5d8c7ba
Fellipe Saraiva <fellipe.saraiva@netscanner.com.br>
2021-06-12 15:22:03 -0300

    att funcoes

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,4 @@
-
+var auxiliar = "";
 function validar(){
 
     var palavra = $('#palavra').val();
@@ -54,7 +54,7 @@ function criptografar(palavra,p,q,e){
 
 function descriptografar(palavra,p,q,e){
     var teste,d,teste3,teste4;
-
+    var einicial = e;
     arraySeparacao = [];
     var u = 0; // for para quebrar a palavra em 4 posições
     for (var i = 0; i < palavra.length; i = i+4){
@@ -69,40 +69,75 @@ function descriptografar(palavra,p,q,e){
     arrayResultados = [];
     
     var aux = "";
+    var valor = 0;
     var i = 0;
     while(aux != 1){
         if(i == 0){ // Primeiro Loop = Inicio Algoritmo de euclides
             divisao = parseInt((n / e));
             resto = n % e;
-            console.log(n+" = "+e+"*"+divisao+"+"+resto);
+            // console.log(n+" = "+e+"*"+divisao+"+"+resto);
             arrayResultados[i] = n - (e*divisao);
             aux = arrayResultados[i];
+            valor = parseInt(n / (-e));
+            ninicial = n;
         }else{
             divisao = parseInt(e / aux);
             resto = e % aux;
-            console.log(e+" = "+divisao+"*"+aux+"+"+resto);
+            // console.log(e+" = "+divisao+"*"+aux+"+"+resto);
             arrayResultados[i] = e - (divisao*aux);
             e = aux;
             aux = arrayResultados[i];
             
         }
         i++;
-        if(i == 6){
-            break;
-        }
     }
 
-    // teste2 = Math.round((n / e),0);
-    // d = e * teste2;
-    // console.log(d);
-    // if(d  < 0)
-    //     teste3 = e - d;
-    // teste4 = Math.pow(u+1,teste3);
-    // console.log(d);
+    if(arrayResultados.length == 1){
+        d = valor;
+        if(d < 0){
+            d = d + ninicial;
+        }
+        
+    }
 
+    for(var i = 0; i < arraySeparacao.length; i++){
+        chamadaApi(arraySeparacao[i]+"^"+d+" \\equiv M mod "+(p*q));
+    }
 
 }
 
+function chamadaApi(input){
+    //418^1949mod2537
+    $.ajax({
+        url  : 'https://api.wolframalpha.com/v2/query?input='+input+'&format=plaintext&output=JSON&&appid=XTA64W-J9AL3QPYJE',
+        data : 'cliente=eu&acao=getmenu',
+        type : "GET",
+        crossDomain  : "true",
+        dataType     : "jsonp",
+        contentType  : "application/json",
+        async: false,
+        success: function( menu ){
+            jsonResultado = menu;
+            auxiliar = jsonResultado.queryresult.pods[1].subpods[0].plaintext;
+            string = auxiliar;
+            string = string.split("M congruent ");
+            string = string[1];
+            string = string.split(" (mod");
+
+            if(parseInt(string[0]) < 1000){
+                string[0] = "0"+string[0];
+            }
+
+            for (var i = 0; i < string[0].length; i+=2){
+                stringaux = string[0].substring(i,i+2);
+                console.log(stringaux);
+                $('#resultado').val($('#resultado').val() + dicionarioInverso(stringaux));
+            }
+           
+            console.log(string[0]);
+        }
+    });
+}
 // Cripto -> Letras > A = 00, B = 01, ~ Z = 35
 
 function dicionario(letra){
@@ -158,6 +193,63 @@ function dicionario(letra){
         return "24";
     else if(letra == "z")
         return "25";
+    else 
+        return false;  
+}
+
+function dicionarioInverso(numero){
+    if(numero == "00")
+        return "a";
+    else if(numero == "01")
+        return "b";
+    else if(numero == "02")
+        return "c";
+    else if(numero == "03")
+        return "d";
+    else if(numero == "04")
+        return "e";
+    else if(numero == "05")
+        return "f";
+    else if(numero == "06")
+        return "g";
+    else if(numero == "07")
+        return "h";
+    else if(numero == "08")
+        return "i";
+    else if(numero == "09")
+        return "j";
+    else if(numero == "10")
+        return "k";
+    else if(numero == "11")
+        return "l";
+    else if(numero == "12")
+        return "m";
+    else if(numero == "13")
+        return "n";
+    else if(numero == "14")
+        return "o";
+    else if(numero == "15")
+        return "p";
+    else if(numero == "16")
+        return "q";
+    else if(numero == "17")
+        return "r";
+    else if(numero == "18")
+        return "s";
+    else if(numero == "19")
+        return "t";
+    else if(numero == "20")
+        return "u";
+    else if(numero == "21")
+        return "v";
+    else if(numero == "22")
+        return "w";
+    else if(numero == "23")
+        return "x";
+    else if(numero == "24")
+        return "y";
+    else if(numero == "25")
+        return "z";
     else 
         return false;  
 }
